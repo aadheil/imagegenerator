ca56c9eef9a18c0df26643a09bc1574633305b98
aadheil <adilkkinbox@gmail.com>
2024-02-12 17:10:11 +0530

    done5

--- src/Pythonapi.jsx
+++ src/Pythonapi.jsx
@@ -3,9 +3,11 @@ import { callpythonnapi } from './Services/allApi';
 
 function Pythonapi() {
     const[apidata,setapidat]=useState()
+    var videolink
     const callpythonapi=async()=>{
         const res=await callpythonnapi()
         setapidat(res)
+        videolink=res.data[0].video_link
         console.log(res);
     }
     useEffect(()=>{
@@ -15,7 +17,7 @@ function Pythonapi() {
     <>
     <h2>{apidata?.data[0].video_title}</h2>
     <video controls width="400" style={{marginTop:'50px'}}>
-                <source src={apidata?.data[0].video_link} type="video/mp4" />
+                <source src={videolink} type="video/mp4" />
                 Your browser does not support the video tag.
               </video>
     </>
