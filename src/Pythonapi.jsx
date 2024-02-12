074ffc096d37be808850a4c1e9cf11c20c621d03
aadheil <adilkkinbox@gmail.com>
2024-02-12 17:06:55 +0530

    done4:

--- src/Pythonapi.jsx
+++ src/Pythonapi.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { callpythonnapi } from './Services/allApi';
 
 function Pythonapi() {
+    const[apidata,setapidat]=useState()
     const callpythonapi=async()=>{
         const res=await callpythonnapi()
+        setapidat(res)
         console.log(res);
     }
     useEffect(()=>{
@@ -11,9 +13,9 @@ function Pythonapi() {
     },[])
   return (
     <>
-    <h2>{res.data[0].video_title}</h2>
+    <h2>{apidata?.data[0].video_title}</h2>
     <video controls width="400" style={{marginTop:'50px'}}>
-                <source src={res.data[0].video_link} type="video/mp4" />
+                <source src={apidata?.data[0].video_link} type="video/mp4" />
                 Your browser does not support the video tag.
               </video>
     </>
